c6dc3a10619faa808326b0c4cd603e820bbd9f32
zaddyParth <jpartha210@gmail.com>
2022-04-19 10:48:35 +0530

    Updates

---
+++ precache-manifest.80a1dd7a34c6bcfc57da36eefcd8015a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "248d5fae3f89b2a2ee1130cc8c20637f",
+    "url": "/task-management-frontend/index.html"
+  },
+  {
+    "revision": "caab847d81fd9248f6f0",
+    "url": "/task-management-frontend/static/css/main.4cbb8e2c.chunk.css"
+  },
+  {
+    "revision": "c138707727720de44b06",
+    "url": "/task-management-frontend/static/js/2.78c34cbe.chunk.js"
+  },
+  {
+    "revision": "caab847d81fd9248f6f0",
+    "url": "/task-management-frontend/static/js/main.fcd295be.chunk.js"
+  },
+  {
+    "revision": "170cb679b8a6db4918f5",
+    "url": "/task-management-frontend/static/js/runtime~main.961a32b0.js"
+  }
+]);
